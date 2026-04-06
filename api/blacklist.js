254ad398b48db1aab832f9ce63d4325234134f76
mosakowskibartle-tech <mosakowskibartle@gmail.com>
2026-04-06 15:07:18 +0300

    234

--- api/blacklist.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  if (req.method === 'OPTIONS') return res.status(204).end();
-  return res.status(404).json({ error: 'Not found' });
-}
